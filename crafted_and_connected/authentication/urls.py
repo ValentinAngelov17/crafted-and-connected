76f656a6b4e65ffacbfecf66b8b1171e724f673c
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2024-05-31 20:52:19 +0300

    added functionality for view others people profiles

--- crafted_and_connected/authentication/urls.py
+++ crafted_and_connected/authentication/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from crafted_and_connected.authentication.views import CustomUserRegistrationView, CustomLoginView, profile, \
-    logout_view, update_profile_picture
+    logout_view, update_profile_picture, user_profile
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('register/', CustomUserRegistrationView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('profile/', profile, name='profile'),
+    path('profile/<int:user_id>/', user_profile, name='user_profile'),  # For viewing other user profiles
+    path('profile/', profile, name='profile'),  # For viewing the logged-in user's profile
     path('logout/', logout_view, name='logout'),
     path('update_profile_picture/', update_profile_picture, name='update_profile_picture'),
 ]
--- crafted_and_connected/authentication/views.py
+++ crafted_and_connected/authentication/views.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.urls import reverse_lazy
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.views.generic import CreateView, FormView
 from crafted_and_connected.authentication.forms import CustomUserCreationForm, CustomUserLoginForm, ProfilePictureForm
-from crafted_and_connected.authentication.models import Follow
+from crafted_and_connected.authentication.models import Follow, CustomUser
 from django.contrib.auth.hashers import check_password, make_password
 from crafted_and_connected.social.models import Post
+
+User = get_user_model()
 
 
 # Create your views here.
@@ -52,15 +55,23 @@ class CustomLoginView(LoginView):
 @login_required
 def profile(request):
     user = request.user
+    return redirect('user_profile', user_id=user.id)
 
+
+@login_required
+def user_profile(request, user_id):
+    user = get_object_or_404(User, id=user_id)
     posts = Post.objects.filter(user=user).order_by('-created_at')
     post_count = posts.count()
-    print("Posts:", posts)
     followers_count = Follow.objects.filter(followed=user).count()
     following_count = Follow.objects.filter(follower=user).count()
-    return render(request, 'profile.html',
-                  {'user': user, 'followers_count': followers_count, 'following_count': following_count,
-                   'posts': posts, 'post_count': post_count})
+    return render(request, 'profile.html', {
+        'user': user,
+        'followers_count': followers_count,
+        'following_count': following_count,
+        'posts': posts,
+        'post_count': post_count
+    })
 
 
 @login_required
@@ -83,3 +94,4 @@ def update_profile_picture(request):
     else:
         form = ProfilePictureForm(instance=request.user)
     return render(request, 'update_profile_picture.html', {'form': form})
+
